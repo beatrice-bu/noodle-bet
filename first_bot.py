47fbb6bc2c5be4b16a182e6e43320b052af142e9
Beatrice-bu <beatrice.au.bu@gmail.com>
2021-05-08 14:42:43 -0400

    added more func

--- first_bot.py
+++ first_bot.py
@@ -2,11 +2,31 @@ import discord
 import json
 import os
 from numpy.random import rand
+import string
+import random
 client = discord.Client()
 
 with open('token.txt', 'r') as t:
     token = t.read()
 
+bets = {}
+
+def id_gen():
+    return ''.join(random.choice(string.ascii_letters) for x in range(5))
+    
+class Bet:
+    
+    def __init__(self, title, users, pot, wager):
+        self.title = title
+        self.users = users
+        self.pot = pot
+        self.wager = wager
+        self.id = id_gen()
+        
+    def announce(self):
+        return f'Title: {self.title}, users: {self.users}, pot: {self.pot}, wager: {self.wager}, id: {id}'    
+        
+        
 @client.event
 async def on_ready():
     print("We have logged in as {0.user}".format(client))
@@ -34,12 +54,22 @@ async def on_message(message):
         await message.channel.send(f"Goodbye, {message.author.display_name}.")
         await unregister_user(message.author.id)
         
-        
+    
     if message.content.startswith('!noodles'):
         print(f'Checking how many noodles {message.author} has...')
         await message.channel.send(f'Hello, {message.author.display_name}. You have {noodles(message.author.id)} noodles.') 
 
-
+    
+    if message.content.startswith('!bet'):
+    
+        bet_title = message.content[5:]
+        print(f'NEW BET: {bet_title}')
+        bets[bet_title] = Bet(bet_title, [message.author], 10, 10)
+        
+        
+    if message.content.startswith('!openbets'):
+        for bet in bets:
+            await message.channel.send(bet)
 async def register_user(user):
     base_noodles = 100
     recovery = rand(3,3,3)
@@ -59,9 +89,6 @@ async def unregister_user(user):
     full_json = id_to_read + '.json'
     if os.path.exists(full_json):
         os.remove(full_json)
-
-
-
 
 
 def noodles(user):
